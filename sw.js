8f846825e5cb386c19e693aa594042dd33e1c50a
heliodoroauthor <heliodoro.author@gmail.com>
2026-06-15 17:07:37 -0500

    chore(sw): bump statsfix-15jun

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 //  • CDNs (Leaflet, fonts): cache-first   (TTL largo)
 //
 // ============================================================
-const VERSION       = 'tequio-v2.0.3-rollback-final2-15jun';
+const VERSION       = 'tequio-v2.0.3-statsfix-15jun';
 const CACHE_SHELL   = `${VERSION}-shell`;
 const CACHE_ASSETS  = `${VERSION}-assets`;
 const CACHE_DATA    = `${VERSION}-data`;
