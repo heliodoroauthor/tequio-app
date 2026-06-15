f6214d24136afcae123d09f19f58c2397387ead0
heliodoroauthor <heliodoro.author@gmail.com>
2026-06-15 12:32:46 -0500

    chore(sw): bump rollback-leyesfix4-15jun

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 //  • CDNs (Leaflet, fonts): cache-first   (TTL largo)
 //
 // ============================================================
-const VERSION       = 'tequio-v2.0.3-rollback-iconfix-15jun';
+const VERSION       = 'tequio-v2.0.3-rollback-leyesfix4-15jun';
 const CACHE_SHELL   = `${VERSION}-shell`;
 const CACHE_ASSETS  = `${VERSION}-assets`;
 const CACHE_DATA    = `${VERSION}-data`;
