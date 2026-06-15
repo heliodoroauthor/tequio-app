cf2c263ea8285b6587be171b3c95c2ba92fdcbb4
heliodoroauthor <heliodoro.author@gmail.com>
2026-06-14 21:51:30 -0500

    chore(sw): bump → onbrevert-14jun

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 //  • CDNs (Leaflet, fonts): cache-first   (TTL largo)
 //
 // ============================================================
-const VERSION       = 'tequio-v2.0.3-onbfix2-14jun';
+const VERSION       = 'tequio-v2.0.3-onbrevert-14jun';
 const CACHE_SHELL   = `${VERSION}-shell`;
 const CACHE_ASSETS  = `${VERSION}-assets`;
 const CACHE_DATA    = `${VERSION}-data`;
